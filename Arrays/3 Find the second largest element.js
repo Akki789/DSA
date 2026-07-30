a8972c49acaf5f7c440045431b614930eebd4006
Akash <aakashkumarag96@gmail.com>
2026-07-30 23:20:34 +0530

    Solved Find the second largest element

---
+++ Arrays/3 Find the second largest element.js
@@ -0,0 +1,24 @@
+//Find the second largest element || Traversal
+
+
+function SecondLargestElement(arr){
+    let largest = -Infinity;
+    let secondLargest = -Infinity;
+
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]>largest){
+            secondLargest = largest;
+            largest = arr[i];
+
+        }else if(arr[i] > secondLargest && arr[i] !== largest){
+            secondLargest = arr[i]
+        }
+    }
+
+    return secondLargest;
+}
+
+let arr = [4, 7, 1, 9, 2]
+
+let second = SecondLargestElement(arr);
+console.log(second);
