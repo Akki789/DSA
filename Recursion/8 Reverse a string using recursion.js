0c683db04ac65874816261a087012331534e711d
Akash <aakashkumarag96@gmail.com>
2026-08-03 19:28:58 +0530

    Reverse a string using recursion

---
+++ Recursion/8 Reverse a string using recursion.js
@@ -0,0 +1,15 @@
+//Reverse a string using recursion
+
+function reverse(str){
+    if(str.length <=1){
+        return str;
+    }
+
+    let res = reverse(str.slice(1)) + str[0];
+    return res;
+}
+
+console.log(reverse("akash"));
+
+//time Complexcity :- O(n2) because slice function takes O(n) and we are calling it n times.
+//space Complexcity :- O(n) because we are using recursion and recursion takes space in stack.
