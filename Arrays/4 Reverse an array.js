475473c6a10b74a585b8cab24348f42da47d9a76
Akash <aakashkumarag96@gmail.com>
2026-08-04 16:43:56 +0530

    Practiced reverse array again

--- Arrays/4 Reverse an array.js
+++ Arrays/4 Reverse an array.js
@@ -1,22 +1,64 @@
 //Reverse an array || Two Pointers
-
 function reverseArray(arr){
-    let left = 0;
-    let right = arr.length-1;
-    
-    
-   while(left < right){
-    let temp = arr[left];
-    arr[left] = arr[right];
-    arr[right] = temp;
+    let left =0;
+    let right  = arr.length-1;
+    // let res = [];
 
-    left++
-    right--
-   }
+    while(left<right){
+        let temp = arr[left];
+        arr[left] = arr[right]
+        arr[right] = temp;
+        left++
+        right--
+    }
 
-   return arr;
+    return arr;
 }
 
-let arr = [5,9,8,3,7,1,6]
-let res = reverseArray(arr);
-console.log(res);
+console.log(reverseArray([1,3,4,5,6,7]));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function reverseArray(arr){
+//     let left = 0;
+//     let right = arr.length-1;
+    
+    
+//    while(left < right){
+//     let temp = arr[left];
+//     arr[left] = arr[right];
+//     arr[right] = temp;
+
+//     left++
+//     right--
+//    }
+
+//    return arr;
+// }
+
+// let arr = [5,9,8,3,7,1,6]
+// let res = reverseArray(arr);
+// console.log(res);
