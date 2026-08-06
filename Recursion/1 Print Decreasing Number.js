874a3463d1b35a64ff121dd4bbbb621e9f2f18c0
Akash <aakashkumarag96@gmail.com>
2026-08-06 14:54:37 +0530

    practiced again Print numbers from N to 1 using recursion

--- Recursion/1 Print Decreasing Number.js
+++ Recursion/1 Print Decreasing Number.js
@@ -1,12 +1,24 @@
 //Print Decreasing Number using recursion || Print numbers from N to 1 using recursion
 
-function printDecreasing(n){
-    if(n===0){
+// function printDecreasing(n){
+//     if(n===0){
+//         return;
+//     }
+
+//     console.log(n);
+//     printDecreasing(n-1);
+// }
+
+// printDecreasing(5);
+
+function printDecrease(n){
+    if(n==0){
         return;
     }
 
     console.log(n);
-    printDecreasing(n-1);
+    printDecrease(n-1)
 }
 
-printDecreasing(5);
+printDecrease(5);
+
