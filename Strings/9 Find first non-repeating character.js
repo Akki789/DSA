190ff8c84af9589d4ad45d0eb2cec74254654a46
Akash <aakashkumarag96@gmail.com>
2026-08-21 21:23:50 +0530

    Solved Find first non-repeating character

---
+++ Strings/9 Find first non-repeating character.js
@@ -0,0 +1,15 @@
+//Find first non-repeating character
+function findChar(str){
+    let freq={};
+    for(let ch of str){
+        freq[ch] = (freq[ch] || 0) +1;
+    }
+
+    for(let ch of str){
+        if(freq[ch] == 1){
+            return ch;
+        }
+    }
+    return null
+}
+console.log(findChar("akash"));
